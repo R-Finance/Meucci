d7e508d0787f501464dd2fac4777e3e68ba43acf
Xavier Valls <flamejat@gmail.com>
2013-07-29 12:18:31 +0000

    -added two demo scripts from chapter 3 and one from chapter 4. Fixed documentation

---
+++ demo/S_Toeplitz.R
@@ -0,0 +1,35 @@
+#' This script shows that the eigenvectors of a Toeplitz matrix have a Fourier basis structure under t-distribution
+#' assumptions, as described in A. Meucci, "Risk and Asset Allocation", Springer, 2005,  Chapter 3.
+#'
+#' @references
+#' \url{http://symmys.com/node/170}
+#' See Meucci's script for "S_Toeplitz.R"
+#'
+#' @author Xavier Valls \email{flamejat@@gmail.com}
+
+
+###############################################################################################################
+### Inputs
+
+N = 200; # dimension of the matrix
+Decay = 0.9; # decay factor
+
+###############################################################################################################
+T = diag( 1, N);
+for( n in 1 : (N - 1) )
+{
+
+    T = T + Decay^n * ( cbind(  matrix( 0, N, N -( N-n ) ), diag( 1, N , N-n) ) +
+      cbind( rbind( matrix(0, N-(N-n), N-n ), diag( 1, N-n)), matrix(0, N, N-(N-n) ) )) ;
+
+
+}
+eig = eigen( T );
+
+###############################################################################################################
+
+#R sorts the eigen vectors, so the results aren't going to be exactly the same as in MATLAB
+
+dev.new();
+plot( eig$vectors[ , n ], type = "l", col = runif(1)*100 );
+lines( eig$vectors[ , n-1 ], type = "l", col = runif(1)*100 );
---
+++ demo/S_VolatilityClustering.R
@@ -0,0 +1,30 @@
+#' This file generates paths for a volatility clustering, as described in A. Meucci, "Risk and Asset Allocation",
+#' Springer, 2005,  Chapter 3.
+#'
+#' @references
+#' \url{http://symmys.com/node/170}
+#' See Meucci's script for "S_VolatilityClustering.m"
+#'
+#' @author Xavier Valls \email{flamejat@@gmail.com}
+##################################################################################################################
+### Input parameters
+mu = 0.05; # mean
+a  = 0.03;
+b  = 0.96;
+s  = 0.01;
+T  = 1000;
+
+##################################################################################################################
+### Simulate path
+z = rnorm(T);
+s2 = s^2;
+eps = array( NaN, T );
+eps[ 1 ] = s2;
+for( t in 1 : (T - 1) )
+{
+    s2[ t + 1 ]  = s^2 + a * ( z[ t ]^2) + b * s2[ t ];
+    eps[ t + 1 ] = mu + sqrt( s2[ t + 1] ) * z[ t + 1 ];
+}
+
+dev.new();
+plot(eps, type = "l", main = "GARCH(1,1) process vs. time", xlab = "", ylab = "" );
